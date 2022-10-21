320e294960a1b5499a547b1780d6ffc22d7ce28d
hungnguyen50497 <nguyenhung50497@gmail.com>
2022-10-21 13:51:34 +0700

    them

--- Ứng dụng quản lý sản phẩm/script.js
+++ Ứng dụng quản lý sản phẩm/script.js
@@ -1,7 +1,30 @@
+let table = '';
+let row = '';
+let array = [];
+let array1 = [];
+let x = 0;
+let amounts = 0;
+function displayProduct(product) {
+    amounts++;
+    row = `<th>${product}</th> <td><button onclick="edit()">Edit</button></td> <td><button onclick="deleteProduct(x)">Delete</button></td> <br>`;
+    array.push(row);
+    array1.push(product);
+    document.querySelector('#resultproduct').innerHTML += array[array.length-1];
+}
+
 function addProduct() {
+    let product = document.querySelector('#product').value;
+    displayProduct(product);
+    document.querySelector('#amounts').innerHTML = amounts;
 
 }
-function displayProduct() {
-     let array = [];
 
+function edit() {
+
+}
+
+function deleteProduct(x) {
+
+    amounts--;
+    document.querySelector('#amounts').innerHTML = amounts;
 }
