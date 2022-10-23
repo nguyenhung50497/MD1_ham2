141a9c947f13eb06323914e3ef2f9485c8c9bc5a
hungnguyen50497 <nguyenhung50497@gmail.com>
2022-10-23 16:06:50 +0700

    hoan thanh

--- Ứng dụng quản lý sản phẩm/script.js
+++ Ứng dụng quản lý sản phẩm/script.js
@@ -1,30 +1,45 @@
-let table = '';
 let row = '';
-let array = [];
-let array1 = [];
-let x = 0;
+let array = [1,2,3,4,5,6,7,8,9];
 let amounts = 0;
-function displayProduct(product) {
+function displayProduct() {
     amounts++;
-    row = `<th>${product}</th> <td><button onclick="edit()">Edit</button></td> <td><button onclick="deleteProduct(x)">Delete</button></td> <br>`;
-    array.push(row);
-    array1.push(product);
-    document.querySelector('#resultproduct').innerHTML += array[array.length-1];
+    let str = '';
+    for (let i=0; i<array.length; i++) {
+        str += `<tr id="product${amounts}">
+                <th>${array[i]}</th> 
+                <td><button onclick="editProduct(`+ i +`)">Edit</button></td> 
+                <td><button onclick="deleteProduct(`+ i +`)">Delete</button></td>
+           </tr>`;
+    }
+    document.querySelector('#resultproduct').innerHTML = str;
 }
+displayProduct();
 
 function addProduct() {
     let product = document.querySelector('#product').value;
+    array.push(product);
     displayProduct(product);
     document.querySelector('#amounts').innerHTML = amounts;
-
+    document.querySelector('#product').value = '';
 }
 
-function edit() {
-
+function editProduct(inDex) {
+    let str = '';
+    str = `<input type="text" id="editProduct" value="`+array[inDex]+`">
+            <button onclick="saveProduct(`+ inDex +`)">Save</button>`;
+    document.querySelector('#div2').innerHTML = str;
 }
 
-function deleteProduct(x) {
+function saveProduct(inDex) {
+    array[inDex] = document.querySelector('#editProduct').value;
+    displayProduct();
+    document.querySelector('#div2').innerHTML = '';
+}
 
+function deleteProduct(inDex) {
     amounts--;
     document.querySelector('#amounts').innerHTML = amounts;
+    array.splice(inDex, 1);
+    displayProduct();
+
 }
